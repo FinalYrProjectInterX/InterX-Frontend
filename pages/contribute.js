e5395fa14f594fea5860be4a587ff6f23b38d900
himanisoni05 <himani.soni9099@gmail.com>
2024-05-02 21:56:25 +0530

    new update

--- pages/contribute.js
+++ pages/contribute.js
@@ -5,21 +5,20 @@ import CategorySubmitForm from "@/components/CategorySubmitForm";
 import QAForm from "@/components/QAForm";
 import { useRouter } from "next/router";
 import AdditionalInfo from "@/components/AdditionalInfo";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Footer from "@/components/Footer";
 
 const contribute = ({ user, logout }) => {
   const router = useRouter();
 
-  useEffect(()=>{
-    if(!localStorage.getItem('token')){
-      router.push('/signup');
+  useEffect(() => {
+    if (!localStorage.getItem("token")) {
+      router.push("/signup");
+    } else {
+      settoken(JSON.parse(localStorage.getItem("token")));
     }
-    else{
-      settoken(JSON.parse(localStorage.getItem('token')));
-    }
-  },[])
+  }, []);
 
   // const [name, setname] = useState('');
   // const [contact, setcontact] = useState('');
@@ -70,7 +69,14 @@ const contribute = ({ user, logout }) => {
   //   setQAFormDisplay("none");
   // }
 
-  const onSubmitCategory = (clearedInterview, category, subCategory, categorySlug, urlSlug, additionalFields) => {
+  const onSubmitCategory = (
+    clearedInterview,
+    category,
+    subCategory,
+    categorySlug,
+    urlSlug,
+    additionalFields
+  ) => {
     console.log("clearedInterview", clearedInterview);
     console.log("category", category);
     console.log("subCategory", subCategory);
@@ -111,7 +117,7 @@ const contribute = ({ user, logout }) => {
     setAdditionalInfoDisplay("block");
   };
 
-  const onSubmitAdditionalInfo = async(experience, tips, additionalInfo) => {
+  const onSubmitAdditionalInfo = async (experience, tips, additionalInfo) => {
     console.log("tips++", tips);
     console.log("additionalInfo++", additionalInfo);
     console.log("experience++", experience);
@@ -122,44 +128,55 @@ const contribute = ({ user, logout }) => {
     setcategorySubmitFormDisplay("none");
     setQAFormDisplay("none");
     setAdditionalInfoDisplay("block");
-    const response = await fetch(`${process.env.NEXT_PUBLIC_HOST}/transcripts/create_transcript`, {
-      method: 'POST',
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        token: token,
-        interview_name: clearedInterview,
-        category: category,
-        subCategory: subCategory,
-        optional_subject: optionalSubject?optionalSubject:"",
-        gap_years: gapYears?gapYears:"",
-        year_of_interview: InterviewYear?InterviewYear:"",
-        specialization: specialization?specialization:"",
-        work_experience: workExperience?workExperience:"",
-        exam_scores: catScore?catScore:"",
-        visa_type: visaType?visaType:"",
-        country_applied_for_visa: appliedCountryForVisa?appliedCountryForVisa:"",
-        purpose_of_travel: purposeOfTravel?purposeOfTravel:"",
-        programming_languages: programmingLanguages?programmingLanguages:"",
-        tech_stack_used: techStackUsed?techStackUsed:"",
-        branch: branch?branch:"",
-        commision_type: commissionType?commissionType:"",
-        bank_name: bankName?bankName:"",
-        interview_experience: experience?experience:"",
-        interview_tips: tips?tips:"",
-        additional_info: additionalInfo?additionalInfo:"",
-        rating: 0,
-        category_slug: categorySlug?categorySlug:"",
-        slug: urlSlug?urlSlug:"",
-        status: "Pending",
-        questions_answers: quesans
-      })
-    });
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_HOST}/transcripts/create_transcript`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          token: token,
+          interview_name: clearedInterview,
+          category: category,
+          subCategory: subCategory,
+          optional_subject: optionalSubject ? optionalSubject : "",
+          gap_years: gapYears ? gapYears : "",
+          year_of_interview: InterviewYear
+            ? InterviewYear
+            : admissionYear
+            ? admissionYear
+            : "",
+          specialization: specialization ? specialization : "",
+          work_experience: workExperience ? workExperience : "",
+          exam_scores: catScore ? catScore : "",
+          visa_type: visaType ? visaType : "",
+          country_applied_for_visa: appliedCountryForVisa
+            ? appliedCountryForVisa
+            : "",
+          purpose_of_travel: purposeOfTravel ? purposeOfTravel : "",
+          programming_languages: programmingLanguages
+            ? programmingLanguages
+            : "",
+          tech_stack_used: techStackUsed ? techStackUsed : "",
+          branch: branch ? branch : "",
+          commision_type: commissionType ? commissionType : "",
+          bank_name: bankName ? bankName : "",
+          interview_experience: experience ? experience : "",
+          interview_tips: tips ? tips : "",
+          additional_info: additionalInfo ? additionalInfo : "",
+          rating: 0,
+          category_slug: categorySlug ? categorySlug : "",
+          slug: urlSlug ? urlSlug : "",
+          status: "Pending",
+          questions_answers: quesans,
+        }),
+      }
+    );
     console.log("response+++", response);
     const JSONdata = await response.json();
     console.log("JSONdata+++", JSONdata);
-    if (response.status==200) {
+    if (response.status == 200) {
       toast.success("Transcript Created Successfully!!", {
         position: "top-left",
         autoClose: 3000,
@@ -170,8 +187,7 @@ const contribute = ({ user, logout }) => {
         progress: undefined,
         theme: "light",
       });
-    }
-    else{
+    } else {
       toast.error("Some Error Occured!!", {
         position: "top-left",
         autoClose: 3000,
@@ -187,34 +203,34 @@ const contribute = ({ user, logout }) => {
 
   return (
     <>
-    <div className="h-auto">
-      <ToastContainer />
-      <Navbar user={user} logout={logout} />
-      <div className="flex flex-col items-start justify-center my-12 mx-40">
-        <p className="text-4xl my-2">
-          Thanks for Giving your time to Share your interview experience with
-          us!!
-        </p>
-        <p className="text-xl my-2">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores,
-          nostrum! Minima veritatis velit asperiores fugit provident, dolore
-          rerum quam, commodi cum saepe possimus id incidunt laborum. Quas,
-          enim? Rem obcaecati in eos.
-        </p>
+      <div className="h-auto">
+        <ToastContainer />
+        <Navbar user={user} logout={logout} />
+        <div className="flex flex-col items-start justify-center my-12 mx-40">
+          <p className="text-4xl my-2">
+            Thanks for Giving your time to Share your interview experience with
+            us!!
+          </p>
+          <p className="text-xl my-2">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Asperiores, nostrum! Minima veritatis velit asperiores fugit
+            provident, dolore rerum quam, commodi cum saepe possimus id incidunt
+            laborum. Quas, enim? Rem obcaecati in eos.
+          </p>
+        </div>
+        <div className="" style={{ display: categorySubmitFormDisplay }}>
+          <CategorySubmitForm onSubmitCategory={onSubmitCategory} />
+        </div>
+        <div className="" style={{ display: QAFormDisplay }}>
+          <QAForm onSubmit={onSubmit} />
+        </div>
+        <div className="" style={{ display: AdditionalInfoDisplay }}>
+          <AdditionalInfo onSubmitAdditionalInfo={onSubmitAdditionalInfo} />
+        </div>
       </div>
-      <div className="" style={{ display: categorySubmitFormDisplay }}>
-        <CategorySubmitForm onSubmitCategory={onSubmitCategory} />
+      <div className="mt-32">
+        <Footer />
       </div>
-      <div className="" style={{ display: QAFormDisplay }}>
-        <QAForm onSubmit={onSubmit} />
-      </div>
-      <div className="" style={{ display: AdditionalInfoDisplay }}>
-        <AdditionalInfo onSubmitAdditionalInfo={onSubmitAdditionalInfo} />
-      </div>
-    </div>
-    <div className="mt-32">
-      <Footer/>
-    </div>
     </>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,95 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import styles from '../styles/index.module.css';
-import Link from 'next/link';
+import styles from "../styles/index.module.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className="min-h-screen">
+    <div className={`min-h-screen ${styles.himani} `}>
       <div className="mx-auto max-w-5xl px-6 lg:px-8">
         <div className="mx-auto flex flex-col justify-center items-center min-h-screen">
           <div className="mb-20">
-            <h2 className="text-5xl font-bold tracking-tight text-white sm:text-6xl text-center">InterX</h2>
-            <p className="mt-4 text-lg leading-8 text-gray-300 text-center text-3xl">Nostrud amet eu ullamco nisi aute in ad minim nostrud adipisicing velit quis. Duis tempor incididunt dolore. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est beatae sunt maxime pariatur? Aut at quasi ducimus impedit provident corporis iste magnam aliquam. Aperiam voluptatibus sit iure minus id excepturi beatae sed.</p>
+            <h2 className="text-5xl font-bold tracking-tight text-white sm:text-6xl text-center">
+              InterX
+            </h2>
+            <p className="mt-4 text-lg leading-8 text-gray-300 text-center text-3xl">
+              Nostrud amet eu ullamco nisi aute in ad minim nostrud adipisicing
+              velit quis. Duis tempor incididunt dolore. Lorem, ipsum dolor sit
+              amet consectetur adipisicing elit. Est beatae sunt maxime
+              pariatur? Aut at quasi ducimus impedit provident corporis iste
+              magnam aliquam. Aperiam voluptatibus sit iure minus id excepturi
+              beatae sed.
+            </p>
           </div>
-          <dl className="flex items-center justify-around">
-            <Link href="/read" className={`flex flex-col items-start w-[30%] ${styles.blockstyle}`}>
+          <dl className="flex items-end justify-around">
+            {/* <Link
+              href="/read"
+              className={`flex flex-col items-start w-[30%] ${styles.blockstyle}`}
+            >
               <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10 sujal">
-                <svg className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
+                <svg
+                  className="h-6 w-6 text-white"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z"
+                  />
                 </svg>
               </div>
               <dt className="mt-4 font-semibold text-white">Read</dt>
-              <dd className="mt-2 leading-7 text-gray-400">Non laboris consequat cupidatat laborum magna. Eiusmod non irure cupidatat duis commodo amet.</dd>
-            </Link>
-            <Link href="/contribute" className={`flex flex-col items-start w-[30%] ${styles.blockstyle}`}>
+              <dd className="mt-2 leading-7 text-gray-400">
+                Non laboris consequat cupidatat laborum magna. Eiusmod non irure
+                cupidatat duis commodo amet.
+              </dd>
+            </Link> */}
+            {/* <Link
+              href="/contribute"
+              className={`flex flex-col items-start w-[30%] ${styles.blockstyle}`}
+            >
               <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
-                <svg className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M10.05 4.575a1.575 1.575 0 10-3.15 0v3m3.15-3v-1.5a1.575 1.575 0 013.15 0v1.5m-3.15 0l.075 5.925m3.075.75V4.575m0 0a1.575 1.575 0 013.15 0V15M6.9 7.575a1.575 1.575 0 10-3.15 0v8.175a6.75 6.75 0 006.75 6.75h2.018a5.25 5.25 0 003.712-1.538l1.732-1.732a5.25 5.25 0 001.538-3.712l.003-2.024a.668.668 0 01.198-.471 1.575 1.575 0 10-2.228-2.228 3.818 3.818 0 00-1.12 2.687M6.9 7.575V12m6.27 4.318A4.49 4.49 0 0116.35 15m.002 0h-.002" />
+                <svg
+                  className="h-6 w-6 text-white"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M10.05 4.575a1.575 1.575 0 10-3.15 0v3m3.15-3v-1.5a1.575 1.575 0 013.15 0v1.5m-3.15 0l.075 5.925m3.075.75V4.575m0 0a1.575 1.575 0 013.15 0V15M6.9 7.575a1.575 1.575 0 10-3.15 0v8.175a6.75 6.75 0 006.75 6.75h2.018a5.25 5.25 0 003.712-1.538l1.732-1.732a5.25 5.25 0 001.538-3.712l.003-2.024a.668.668 0 01.198-.471 1.575 1.575 0 10-2.228-2.228 3.818 3.818 0 00-1.12 2.687M6.9 7.575V12m6.27 4.318A4.49 4.49 0 0116.35 15m.002 0h-.002"
+                  />
                 </svg>
               </div>
               <dt className="mt-4 font-semibold text-white">Contribute</dt>
-              <dd className="mt-2 leading-7 text-gray-400">Officia excepteur ullamco ut sint duis proident non adipisicing. Voluptate incididunt anim.</dd>
-            </Link>
+              <dd className="mt-2 leading-7 text-gray-400">
+                Officia excepteur ullamco ut sint duis proident non adipisicing.
+                Voluptate incididunt anim.
+              </dd>
+            </Link> */}
           </dl>
         </div>
       </div>
-      <div className="absolute left-1/2 top-0 -z-10 -translate-x-1/2 blur-3xl xl:-top-6" aria-hidden="true">
-        <div className="aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30" style={{"clip-path": "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"}}></div>
+      <div
+        className="absolute left-1/2 top-0 -z-10 -translate-x-1/2 blur-3xl xl:-top-6"
+        aria-hidden="true"
+      >
+        <div
+          className="aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
+          style={{
+            "clip-path":
+              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+          }}
+        ></div>
       </div>
     </div>
   );
